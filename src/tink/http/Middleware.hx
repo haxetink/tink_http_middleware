e0a12047df678abbc8b9f7d58e0c1d4346369384
Kevin Leung <kevinresol@gmail.com>
2016-12-09 19:29:53 +0800

    Add middleware

---
+++ src/tink/http/Middleware.hx
@@ -0,0 +1,26 @@
+package tink.http;
+
+import tink.http.Handler;
+
+typedef MiddlewareFunction = Handler->Handler;
+
+@:forward
+abstract Middleware(MiddlewareObject) from MiddlewareObject to MiddlewareObject {
+  @:from
+  public static inline function ofFunc(f:MiddlewareFunction):Middleware
+    return new SimpleMiddleware(f);
+}
+
+class SimpleMiddleware implements MiddlewareObject {
+  var f:MiddlewareFunction;
+  
+  public function new(f)
+    this.f = f;
+    
+  public function apply(handler:Handler):Handler
+    return f(handler);
+}
+
+interface MiddlewareObject {
+	function apply(handler:Handler):Handler;
+}

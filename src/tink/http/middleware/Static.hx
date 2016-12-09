7b6ac656ba4f3d0d905e7259f1164ba1a2722214
Kevin Leung <kevinresol@gmail.com>
2016-12-09 15:06:25 +0800

    Support Range header

--- src/tink/http/middleware/Static.hx
+++ src/tink/http/middleware/Static.hx
@@ -1,9 +1,21 @@
 package tink.http.middleware;
 
+import haxe.io.Bytes;
 import tink.http.Request;
+import tink.http.Header;
 import tink.http.Response;
 import tink.http.Middleware;
 import tink.http.Handler;
+
+#if asys
+import asys.io.File;
+import asys.FileSystem;
+import asys.FileStat;
+#elseif sys
+import sys.io.File;
+import sys.FileSystem;
+import sys.FileStat;
+#end
 
 using StringTools;
 using tink.CoreApi;
@@ -43,20 +55,25 @@ class StaticHandler implements HandlerObject {
 		if(path.startsWith(prefix)) {
 			var staticPath = '$root/' + path.substr(prefix.length);
 			#if asys
-				var result:Promise<OutgoingResponse> = asys.FileSystem.exists(staticPath) >>
+				var result:Promise<OutgoingResponse> = FileSystem.exists(staticPath) >>
 					function(exists:Bool)
-						return if(exists) {
-							var mime = mime.Mime.lookup(staticPath);
-							asys.io.File.getBytes(staticPath) >>
-								function(bytes:haxe.io.Bytes) return OutgoingResponse.blob(bytes, mime);
-						} else
+						return if(exists)
+							FileSystem.stat(staticPath) >>
+								function(stat:FileStat) return File.getBytes(staticPath) >>
+								function(bytes:Bytes) {
+									var mime = mime.Mime.lookup(staticPath);
+									return partial(req.header, stat, bytes, mime);
+								}
+						else
 							Future.sync(Failure(notFound));
 							
 				return result.recover(function(_) return handler.process(req));
 			#elseif sys
-				if(sys.FileSystem.exists(staticPath)) {
+				if(FileSystem.exists(staticPath)) {
 					var mime = mime.Mime.lookup(staticPath);
-					return Future.sync(OutgoingResponse.blob(sys.io.File.getBytes(staticPath), mime));
+					var stat = FileSyatem.stat(staticPath);
+					var bytes = File.getBytes(staticPath);
+					return Future.sync(partial(req.header, stat, bytes, mime));
 				}
 			#else
 				#error "Not supported"
@@ -64,5 +81,40 @@ class StaticHandler implements HandlerObject {
 		} 
 		
 		return handler.process(req);
+	}
+	
+	function partial(header:Header, stat:FileStat, bytes:Bytes, contentType:String) {
+		
+		var headers = [
+			new HeaderField('accept-ranges', 'bytes'),
+			new HeaderField('vary', 'Accept-Encoding'),
+			new HeaderField('last-modified', stat.mtime),
+		];
+		
+		// ref: https://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.35
+		switch header.byName('range') {
+			case Success(v):
+				switch (v:String).split('=') {
+					case ['bytes', range]:
+						inline function res(pos, len)
+							return OutgoingResponse.blob(206, bytes.sub(pos, len), contentType, headers.concat([
+								new HeaderField('content-range', 'bytes $pos-${pos + len - 1}/${bytes.length}'),
+							]));
+							
+						switch range.split('-') {
+							case ['', Std.parseInt(_) => len]:
+								return res(bytes.length - len, len);
+							case [Std.parseInt(_) => pos, '']:
+								return res(pos, bytes.length - pos);
+							case [Std.parseInt(_) => pos, Std.parseInt(_) => end]:
+								return res(pos, end - pos + 1);
+							default: // unrecognized byte-range-set (should probably return an error)
+						}
+					default: // unrecognized bytes-unit (should probably return an error)
+				}
+				
+			case Failure(_):
+		}
+		return OutgoingResponse.blob(bytes, contentType, headers);
 	}
 }
